a4d9a0e8e772750c25920215a014e1add6441d46
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-02-28 22:44:49 +0200

    added sprite.png

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,19 +2,26 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.util.Random;
 
 public class GameScreen extends JPanel{
+    private Random random;
 
     // Constructor
     public GameScreen() {
-
+        random = new Random();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // This is required to draw the background and other things
 
-        g.setColor(Color.RED);
-        g.fillRect(50, 50, 100, 100); // Draw a rectangle
+
+        for (int x = 0; x < 20; x++) {
+            for (int y = 0; y < 20; y++) {
+                g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+                g.fillRect(x * 32, y * 32, 32, 32);
+            }
+        }
 
     }
 
